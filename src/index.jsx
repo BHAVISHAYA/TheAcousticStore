77589dc1a3d33601602ecbb1ba13fbdd00b20a96
BHAVISHAYA <bhavishayakhandelwal@gmail.com>
2024-09-30 22:22:15 +0530

    Updating the Environmental Variables

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,13 @@ import { FilterContextProvider } from './context/filter_context.jsx';
 import { CartProvider } from './context/cart_context.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-// const domain = process.env.REACT_APP_AUTH_DOMAIN;
-// const clientId = process.env.RDACT_APP_CLIENT_ID;
+const myDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const myClientId = process.env.REACT_APP_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Auth0Provider
-        domain="dev-u4635ba44bcf6cii.us.auth0.com"
-        clientId="lvkvWiJsxUJAFkclHcE0ZVrVmjpDmA7G"
+        domain={myDomain}
+        clientId={myClientId}
         authorizationParams={{
         redirect_uri: window.location.origin
         }}
