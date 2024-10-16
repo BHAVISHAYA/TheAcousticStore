ebf8e304a1e4e2c869445ee810144a2ac9956945
BHAVISHAYA <bhavishayakhandelwal@gmail.com>
2024-10-16 12:05:33 +0530

    Final Updation

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,16 @@ import { FilterContextProvider } from './context/filter_context.jsx';
 import { CartProvider } from './context/cart_context.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const domainName = import.meta.env.VITE_AUTH_DOMAIN;
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
+// domain="dev-u4635ba44bcf6cii.us.auth0.com"
+// clientId="lvkvWiJsxUJAFkclHcE0ZVrVmjpDmA7G"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Auth0Provider
-        domain="dev-u4635ba44bcf6cii.us.auth0.com"
-        clientId="lvkvWiJsxUJAFkclHcE0ZVrVmjpDmA7G"
+        domain={domainName}
+        clientId={clientId}
         authorizationParams={{
         redirect_uri: window.location.origin
         }}
